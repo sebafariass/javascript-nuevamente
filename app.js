101153f09982c5378af4151ebc64a1826ac22670
sebastian <bioreciclin@gmail.com>
2020-04-10 16:39:07 -0300

    javascript inicio

---
+++ app.js
@@ -0,0 +1,51 @@
+//tipos de datos
+
+
+//numerico - float 
+// con decimal se llaman float y nº enteros integer
+
+
+var edad = 19 / 2; 
+
+
+//string
+
+var nombre= "sebastian";
+var apellido = "ignacio"
+/* dobles comillas no pueden ir continuas para eso
+existen opciones o usamos comillas simples ( ' ' ) 
+o dobles ( " ") o tambien se puede usar (\"  texto\")*/
+
+
+var texto = 'Los videos son los "mejores" '
+
+
+//Array - Arreglos
+
+var familia =["seba" , "juan" , "sandra" , "cristian"];
+
+
+//objetc - objetos
+
+var objeto = { 
+    color : "Red",
+    size : 12
+}
+
+//boolean - boleano
+
+var boleano = true;
+
+//undefined - indefinido
+
+var duda;
+document.write(duda);
+
+//null - nulo 
+
+var numero2 = 10; 
+var numero2 = null; 
+
+//NaN
+var texto2 = " hola como estas ";
+document.write(texto2 * 2);
